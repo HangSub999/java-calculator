dba53dbf006749308d447103fcbceb060e91f034
HangSub999 <gkdtjq999@gmail.com>
2022-11-26 00:09:12 +0900

    feat: 사용자 입력 기능

--- src/src/view/Input.java
+++ src/src/view/Input.java
@@ -1,4 +1,13 @@
 package view;
 
+import java.util.Scanner;
+
 public class Input {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String InputUser() {
+        System.out.println("계산할 숫자와 부호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
 }
