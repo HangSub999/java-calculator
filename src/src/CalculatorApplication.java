b9038e7405d2c98212c0bd355c22d83ce9e6d8b5
HangSub999 <gkdtjq999@gmail.com>
2022-11-26 16:18:32 +0900

    feat: main에 계산기 만들기

--- src/src/CalculatorApplication.java
+++ src/src/CalculatorApplication.java
@@ -1,5 +1,70 @@
+import controller.CalculatorManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CalculatorApplication {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<String> marks = makeMarkFactory(input);
+        List<Long> numbers = makeNumberFactory(input, marks);
+        long sum = makeFirstNumber(input);
+        System.out.println(makeCalculationNumber(sum, marks, numbers));
     }
+
+    public static long makeCalculationNumber(long sum, List<String> marks, List<Long> numbers) {
+        long tmp = sum;
+        for (int i = 0; i < marks.size(); i++) {
+            if (marks.get(i).equals("+")) {
+                tmp += numbers.get(i + 1);
+            }
+            if (marks.get(i).equals("-")) {
+                tmp -= numbers.get(i + 1);
+            }
+            if (marks.get(i).equals("*")) {
+                tmp *= numbers.get(i + 1);
+            }
+            if (marks.get(i).equals("/")) {
+                tmp /= numbers.get(i + 1);
+            }
+        }
+        return tmp;
+    }
+
+    public static Long makeFirstNumber(String input) {
+        return Long.parseLong(String.valueOf(input.charAt(0)));
+    }
+
+    public static List<Long> makeNumberFactory(String input, List<String> marks) {
+        List<Long> numbers = new ArrayList<>();
+        String[] tmp = input.split("");
+        for (String s : tmp) {
+            if (!marks.contains(s)) {
+                numbers.add(Long.parseLong(s));
+            }
+        }
+        return numbers;
+    }
+
+    public static List<String> makeMarkFactory(String input) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '+') {
+                list.add("+");
+            }
+            if (input.charAt(i) == '-') {
+                list.add("-");
+            }
+            if (input.charAt(i) == '*') {
+                list.add("*");
+            }
+            if (input.charAt(i) == '/') {
+                list.add("/");
+            }
+        }
+        return list;
+    }
+
 }
